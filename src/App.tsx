468fa32bc46de7cb198b367aacda0e4c8170da82
Sachit <sachitdahal33@gmail.com>
2025-03-08 16:59:28 +0545

    whatever man personal project anyway

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
 import { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import AOS from "aos";
 
 import Fallback from "./pages/Fallback";
-
 import IndexPage from "./IndexPage";
 import Layout from "./Layout";
 import { UserContextProvider } from "./UserContext";
+import { pageVariants } from "./transition";
 
 import "aos/dist/aos.css";
 import "./App.css";
 import { Contacts } from "./pages/Contacts";
 import { CV } from "./pages/CV";
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <motion.div variants={pageVariants} initial="initial" animate="animate" exit="exit">
+              <Layout />
+            </motion.div>
+          }
+        >
+          <Route index element={<IndexPage />} />
+          <Route path="*" element={<Fallback />} />
+          <Route
+            path="/contact"
+            element={
+              <motion.div variants={pageVariants} initial="initial" animate="animate" exit="exit">
+                <Contacts />
+              </motion.div>
+            }
+          />
+          <Route
+            path="/cv"
+            element={
+              <motion.div variants={pageVariants} initial="initial" animate="animate" exit="exit">
+                <CV />
+              </motion.div>
+            }
+          />
+        </Route>
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 function App() {
   useEffect(() => {
-    AOS.init({});
+    AOS.init({ duration: 800,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,});
   }, []);
+
   return (
     <Router>
       <UserContextProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<IndexPage />} />
-
-            <Route path="*" element={<Fallback />} />
-            <Route path="/contact" element={<Contacts />} />
-            <Route path="/cv" element={<CV />} />
-          </Route>
-        </Routes>
+        <AnimatedRoutes />
       </UserContextProvider>
     </Router>
   );
